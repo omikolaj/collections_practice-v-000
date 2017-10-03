a910c2a276b752ee6572c25b5bd6ad33af7d0e0b
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-03 10:54:38 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,5 +6,11 @@ def sort_array_desc(arr)
   arr.sort do |a,b|
     b<=>a
   end
+end
 
+def sort_array_char_count(arr)
+  arr.collect do |char|
+    char.sort do |a,b|
+      a.length <=> b.length
+    end
 end
