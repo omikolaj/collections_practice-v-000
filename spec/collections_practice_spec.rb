422e87e5929d680dd4c8a26c9b22baa136ff31d5
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-03 11:17:09 +0000

    automatically backed up by learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -30,6 +30,13 @@ describe 'collections practice' do
     end
   end
 
+  # Advanced
+  describe '#swap_elements_from_to' do
+    it 'swap the index and destination index elements of an array'
+    expect(swap_elements_from_to(["blake","ashley","scott"], 0, 2)).to.eq.(["scott","ashley","blake"])
+    end
+  end
+
   # Question 5
   describe '#reverse_array' do
     it 'reverse the order of an array of integers' do
