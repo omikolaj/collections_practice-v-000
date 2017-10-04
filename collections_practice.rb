bbbd0be8f55efba0f432ad112d6a7816c94eb851
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-04 10:12:24 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -52,7 +52,6 @@ def add_s(arr)
     arr.each_with_index.collect do |element, index|
       if(index != 1)
         arr[index] = element + "s"
-        binding.pry
       end
     end
     return arr
