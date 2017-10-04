b2a4a04dd9cbe538354033355d23a9bd6fda3c3d
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-04 09:25:47 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -27,7 +27,7 @@ end
 def reverse_array(arr)
   i = 0
   while(i < arr.length)
-    arr[i], arr[arr.length] = arr[arr.length], arr[i]
+    arr[i], arr[arr.length-1] = arr[arr.length-1], arr[i]
     i+=1
   end
   return arr
